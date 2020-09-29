69ee5eeda2605ed47770780677927832363e0cce
RAHUL KUMAR <55033230+pctablet505@users.noreply.github.com>
2020-09-30 00:12:58 +0530

    Create Sparse Table Range Maximum Query.py

---
+++ Sparse Table Range Maximum Query.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 24 03:30:14 2020
+
+@author: rahul
+"""
+
+'''Sparse table for range maximum query 
+    time complexity 
+    O(n log(n)) preprocessing
+    O(1) query
+    '''
+import math
+st=[]
+
+def build(arr):
+    global st
+    n=len(arr)
+    k=math.floor(math.log2(n))+1
+    st=[[0]*(k) for i in range(n)]
+    for i in range(n):
+        st[i][0]=arr[i]
+    
+    for j in range(1,k):
+        i=0
+        while i+(1<<j)<=n:
+            st[i][j]=max(st[i][j-1],st[i+(1<<j-1)][j-1])
+            i+=1
+    
+def query(l,r):
+    j=int(math.log2(r-l+1))
+    res=max(st[l][j],st[r-(1<<j)+1][j])
+    return res
+
+build([1,-3,4,6,2,0,3,5,7])
+print(st)
+print(query(1,3))
